0c3ed9548b3e07cae93d55ae12faffc4e7373559
Chen Zhen <heavenzhen999@163.com>
2024-08-24 23:17:49 +0800

    feat: save

---
+++ practice/docker-env/sync-db/bin/sync-pg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###
+ # @Author       : Chen Zhen
+ # @Date         : 2024-08-22 22:26:34
+ # @LastEditors  : Chen Zhen
+ # @LastEditTime : 2024-08-22 22:27:12
+ # @Description  : Sync postgresql database
+### 
+
+
