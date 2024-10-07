9e49e73b3acc2f5ddb07560a69d6ef352e909006
Chen Zhen <heavenzhen999@163.com>
2024-10-08 01:09:41 +0800

    feat(practice/nodejs-service): integrate Eslint

--- practice/nodejs-service/koa/template/app.js
+++ practice/nodejs-service/koa/template/app.js
@@ -2,7 +2,7 @@
  * @Author       : Chen Zhen
  * @Date         : 2024-07-06 23:31:08
  * @LastEditors  : Chen Zhen
- * @LastEditTime : 2024-08-13 01:04:25
+ * @LastEditTime : 2024-10-08 00:26:16
  */
 const fs = require('fs')
 const path = require('path')
@@ -23,11 +23,11 @@ app.use(async (ctx, next) => {
 // response
 const router = new Router()
 router
-  .get('/favicon.ico', async (ctx, next) => {
+  .get('/favicon.ico', async (ctx) => {
     ctx.type = 'image/x-icon'
     ctx.body = fs.readFileSync(path.join(__dirname, 'public', 'favicon.ico'))
   })
-  .get('/', async (ctx, next) => {
+  .get('/', async (ctx) => {
     ctx.body = 'This is the koa service.'
   })
 
--- practice/nodejs-service/nest/template/src/app.controller.ts
+++ practice/nodejs-service/nest/template/src/app.controller.ts
@@ -1,3 +1,9 @@
+/**
+ * @Author       : Chen Zhen
+ * @Date         : 2024-07-07 17:06:46
+ * @LastEditors  : Chen Zhen
+ * @LastEditTime : 2024-10-08 01:05:53
+ */
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
 
